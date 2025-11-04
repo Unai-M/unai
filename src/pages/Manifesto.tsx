9211abc7e8e75035693cdb683c7e3475af5a364a
astrosuka <yegresvoselok@gmail.com>
2025-11-04 01:27:45 -0300

    Add padding to leave space for nav menu

--- src/pages/Manifesto.tsx
+++ src/pages/Manifesto.tsx
@@ -18,7 +18,7 @@ export default function Manifesto() {
       : profile?.manifesto?.es || profile?.manifesto?.en;
 
   return (
-    <div className="m-auto flex w-[80vw] max-w-prose flex-col gap-2">
+    <div className="mx-auto flex w-[80vw] max-w-prose flex-col gap-2 py-24">
       <PortableText
         value={Array.isArray(manifesto) ? manifesto : []}
         components={BlockContentComponent}
