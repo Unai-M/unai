03b4d2d5c527bde0d72ef6767cd6d769401141f1
astrosuka <yegresvoselok@gmail.com>
2025-11-04 14:19:36 -0300

    Add type to project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,25 +2,32 @@ import { urlFor } from "../lib/sanityImageUrl";
 import { motion } from "motion/react";
 import { NavLink } from "react-router";
 import useLanguage from "@/hooks/useLanguage";
+import type { DirectionProjectsListQueryResult } from "@/lib/types";
 
-export default function ProjectCard({ project }) {
+type Project = DirectionProjectsListQueryResult[number];
+
+export default function ProjectCard({ project }: { project: Project }) {
   const { language } = useLanguage();
 
   return (
     <motion.div className="w-fit flex-col gap-4 rounded pt-4">
       <NavLink to={`/${project.slug?.current}`}>
-        <img
-          src={urlFor(project.previewImage?.url)
-            .format("webp")
-            .width(800)
-            .url()}
-          className="rounded rounded-t-xl"
-        />
+        {project?.previewImage?.url && (
+          <img
+            src={urlFor(project.previewImage?.url)
+              .format("webp")
+              .width(800)
+              .url()}
+            className="rounded rounded-t-xl"
+          />
+        )}
       </NavLink>
-      <div className="mt-1 flex items-center gap-8 rounded rounded-b-2xl bg-black/70 px-4">
-        <h3 className="font-thin tracking-wider uppercase">
-          {project.title[language] ?? project.title.es}
-        </h3>
+      <div className="mt-1 flex items-center gap-8 rounded rounded-b-2xl bg-black/70 px-4 py-1">
+        {project.title && (
+          <h3 className="font-thin tracking-wider uppercase">
+            {project.title[language] ?? project.title.es}
+          </h3>
+        )}
         {project.projectType && (
           <span className="font-mono">
             {project.projectType[language] ?? project.projectType.es}
