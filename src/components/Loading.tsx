bc1efa9950ff60bf3750e56863151e11dbe91ee5
astrosuka <yegresvoselok@gmail.com>
2025-11-04 01:28:37 -0300

    Remove loading background

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react";
 
 export default function Laoding() {
   return (
-    <div className="bg-accent/10 fixed inset-0 flex h-screen w-full items-center justify-center">
+    <div className="fixed inset-0 flex h-screen w-full items-center justify-center">
       <div className="animate-spin">
         <Loader />
       </div>
