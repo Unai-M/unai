397edada7bbca200226c3cce10c77fcacf4539ba
astrosuka <yegresvoselok@gmail.com>
2025-11-04 16:46:01 -0300

    Add language change to project info description

--- src/components/ProjectInfo.tsx
+++ src/components/ProjectInfo.tsx
@@ -1,14 +1,21 @@
 import type { ProjectQueryResult } from "@/lib/types";
 import { PortableText } from "@portabletext/react";
 import { urlFor } from "@/lib/sanityImageUrl";
+import useLanguage from "@/hooks/useLanguage";
 
 interface ProjectInfoProps {
   data: ProjectQueryResult | undefined;
 }
 export default function ProjectInfo({ data }: ProjectInfoProps) {
+  const { language } = useLanguage();
+
   return (
     <div className="mt-8 flex w-full flex-col items-center justify-center">
-      {data?.description?.es && <PortableText value={data.description.es} />}
+      {data?.description?.es && (
+        <PortableText
+          value={data.description[language] || data.description.es}
+        />
+      )}
       {data?.credits &&
         data.credits.map((credit) => (
           <div key={credit._key}>{credit.name}</div>
