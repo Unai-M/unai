95a80cc48e5b5d1289e9670f70e27ca279a1c496
astrosuka <yegresvoselok@gmail.com>
2025-11-04 01:37:16 -0300

    Add enter-exit animations and background decoration

--- src/pages/Treatments.tsx
+++ src/pages/Treatments.tsx
@@ -18,48 +18,80 @@ export default function Treatment() {
   const { data, isLoading, error } = useTreatmentProjects();
   const { language } = useLanguage();
 
-  if (isLoading) return <Loading />;
   if (error) return <ErrorPage error={error} />;
 
   return (
-    <motion.section className="flex w-full justify-center sm:mx-24">
-      <Carousel className="rounded-xl bg-white/10 sm:w-3/4">
-        <CarouselContent>
-          {data?.length &&
-            data.map((project) => {
-              const title =
-                language === "en"
-                  ? project.title?.en || project.title?.es
-                  : project.title?.es || project.title?.en;
+    <motion.section
+      initial={{ opacity: 1, scaleX: 0 }}
+      animate={{ opacity: 1, scaleX: 1 }}
+      exit={{
+        opacity: 1,
+        scaleX: 0,
+        transition: { delay: 0.15, duration: 0.5 },
+      }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+      className="no-doc-scroll fixed inset-0 flex w-full origin-right justify-center bg-black"
+    >
+      {isLoading && <Loading />}
 
-              return (
-                <CarouselItem key={project._id} className="">
-                  <div className="flex h-screen flex-col items-center justify-around">
-                    {project.image?.url && (
-                      <img
-                        src={urlFor(project.image.url)
-                          .format("webp")
-                          .width(600)
-                          .url()}
-                        className="_opacity-30 _grayscale _w-[450px]"
-                      />
-                    )}
-                    <div className="flex w-full justify-center">
-                      {project.description?.es && (
-                        <PortableText
-                          value={project.description.es}
-                          components={BlockContentComponent}
+      <motion.div
+        initial={{ opacity: 0, scaleX: 1 }}
+        animate={{ opacity: 1, scaleX: 1 }}
+        transition={{ delay: 0.5, ease: "easeInOut" }}
+        exit={{
+          opacity: 0,
+          scaleX: 1,
+          transition: { duration: 0.15 },
+        }}
+        className="flex w-full justify-center"
+      >
+        {/* grilla de puntos */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3, delay: 1.4 }}
+          exit={{ opacity: 0 }}
+          className="fixed top-[5vh] left-[20vw] h-[95vh] w-[60vw] bg-[radial-gradient(circle_at_1px_1px,oklch(0.9367_0.1179_261.9)_1px,transparent_0)] [background-size:80px_80px]"
+        />
+        <Carousel className="">
+          <CarouselContent>
+            {data?.length &&
+              data.map((project) => {
+                const title =
+                  language === "en"
+                    ? project.title?.en || project.title?.es
+                    : project.title?.es || project.title?.en;
+
+                return (
+                  <CarouselItem key={project._id} className="">
+                    <div className="flex h-screen max-w-full flex-col items-center justify-center gap-4">
+                      {project.image?.url && (
+                        <img
+                          src={urlFor(project.image.url)
+                            .format("webp")
+                            .width(1200)
+                            .url()}
+                          className="max-w-screen rounded"
                         />
                       )}
+
+                      <div className="flex w-full justify-center overflow-auto">
+                        {project.description?.es && (
+                          <PortableText
+                            value={project.description.es}
+                            components={BlockContentComponent}
+                          />
+                        )}
+                      </div>
                     </div>
-                  </div>
-                </CarouselItem>
-              );
-            })}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+                  </CarouselItem>
+                );
+              })}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </motion.div>
     </motion.section>
   );
 }
