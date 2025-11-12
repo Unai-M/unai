ae18a06ee522676d3b9f4830628b7134a8a3d51f
astrosuka <yegresvoselok@gmail.com>
2025-11-12 20:25:33 -0300

    Add navigation to project page functionality

--- src/pages/Archive.tsx
+++ src/pages/Archive.tsx
@@ -3,10 +3,13 @@ import useLanguage from "@/hooks/useLanguage";
 import ErrorPage from "./ErrorPage";
 import { motion } from "motion/react";
 import Loading from "@/components/Loading";
+import { NavLink } from "react-router";
+import { useState } from "react";
 
 export default function Archive() {
   const { data, isLoading, error } = useDirectionProjectsList();
   const { language } = useLanguage();
+  const [isNavigating, setIsNavigating] = useState(false);
 
   if (error) return <ErrorPage error={error} />;
 
@@ -14,45 +17,58 @@ export default function Archive() {
     <motion.section
       initial={{ opacity: 1, scaleX: 0 }}
       animate={{ opacity: 1, scaleX: 1 }}
-      exit={{
-        opacity: 1,
-        scaleX: 0,
-        transition: { delay: 0.15, duration: 0.5 },
-      }}
+      exit={
+        isNavigating
+          ? { opacity: 1, scaleX: 1 }
+          : {
+              opacity: 1,
+              scaleX: 0,
+              transition: { delay: 0.15, duration: 0.5 },
+            }
+      }
       transition={{ duration: 0.5, ease: "easeOut" }}
       className="no-doc-scroll fixed inset-0 z-20 flex origin-left justify-center bg-black py-24"
     >
       {isLoading && <Loading />}
-
       <motion.div
         initial={{ opacity: 0, scaleX: 1 }}
         animate={{ opacity: 1, scaleX: 1 }}
         transition={{ delay: 0.5, ease: "easeInOut" }}
-        exit={{
-          opacity: 0,
-          scaleX: 1,
-          transition: { duration: 0.15 },
-        }}
+        exit={
+          isNavigating
+            ? { opacity: 1, scaleX: 1 }
+            : {
+                opacity: 0,
+                scaleX: 1,
+                transition: { duration: 0.15 },
+              }
+        }
         className={`${data?.length && data.length > 18 ? "border-foreground/50 border-b" : ""} flex w-1/2 flex-col justify-center overflow-y-scroll pb-2`}
       >
         {data?.length &&
           data.map((project) => {
             return (
-              <div key={project._id} className="mb-2 flex items-center gap-8">
-                <span className="font-mono">
-                  {project.date ?? project.date}
-                </span>
-                {project.title && (
-                  <h3 className="font-thin tracking-wider uppercase">
-                    {project.title[language] ?? project.title.es}
-                  </h3>
-                )}
-                {project.projectType && (
-                  <span className="font-mono text-sm">
-                    {project.projectType[language] ?? project.projectType.es}
+              <NavLink
+                key={project._id}
+                to={`/direccion/${project.slug?.current}`}
+                onClick={() => setIsNavigating(true)}
+              >
+                <div className="mb-2 flex items-center gap-8">
+                  <span className="font-mono">
+                    {project.date ?? project.date}
                   </span>
-                )}
-              </div>
+                  {project.title && (
+                    <h3 className="font-thin tracking-wider uppercase">
+                      {project.title[language] ?? project.title.es}
+                    </h3>
+                  )}
+                  {project.projectType && (
+                    <span className="font-mono text-sm">
+                      {project.projectType[language] ?? project.projectType.es}
+                    </span>
+                  )}
+                </div>
+              </NavLink>
             );
           })}
       </motion.div>
