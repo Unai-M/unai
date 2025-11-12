505c1f704c7db8df7bc68066dd98e53fb8d176f5
astrosuka <yegresvoselok@gmail.com>
2025-11-12 20:23:16 -0300

    Remove name

--- src/components/BackgroundDecorations.tsx
+++ src/components/BackgroundDecorations.tsx
@@ -17,16 +17,6 @@ export default function BackgroundDecorations() {
         exit={{ opacity: 0 }}
       />
 
-      <motion.div
-        className="font-display pointer-events-none fixed top-1/2 left-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 scale-x-125 overflow-hidden text-[30vw] font-bold blur-xs select-none"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 0.05 }}
-        transition={{ duration: 1.75, delay: 1.5 }}
-        exit={{ opacity: 0 }}
-      >
-        UNAI
-      </motion.div>
-
       <Lines key="lines" />
     </div>
   );
