b4a1c19e04700fc216261ff19f02089ddceb26f6
astrosuka <yegresvoselok@gmail.com>
2025-11-21 10:42:43 -0300

    Add mobile layout to project info

--- src/components/ProjectInfo.tsx
+++ src/components/ProjectInfo.tsx
@@ -2,16 +2,18 @@ import type { ProjectQueryResult } from "@/lib/types";
 import { PortableText } from "@portabletext/react";
 import useLanguage from "@/hooks/useLanguage";
 import ImageGallery from "./ImageGallery";
+import useIsMobile from "@/hooks/useIsMobile";
 
 interface ProjectInfoProps {
   data: ProjectQueryResult | undefined;
 }
 export default function ProjectInfo({ data }: ProjectInfoProps) {
   const { language } = useLanguage();
+  const isMobile = useIsMobile();
 
   return (
     <div className="mx-auto flex w-[80vw] flex-col items-center justify-center gap-16 pt-16">
-      <div className="grid w-full grid-cols-3 gap-8">
+      <div className="grid w-full grid-cols-1 gap-8 sm:grid-cols-3">
         <div className="col-span-2">
           {data?.description?.es && (
             <PortableText
@@ -24,13 +26,22 @@ export default function ProjectInfo({ data }: ProjectInfoProps) {
           {data?.credits &&
             data.credits.map((credit) => (
               <div key={credit._key}>
-                {credit.role?.es && (
-                  <span className="font-mono text-xs uppercase">
-                    {credit.role[language] || credit.role.es}:{" "}
-                  </span>
+                {credit.role?.es && !isMobile && (
+                  <>
+                    <span className="font-mono text-xs uppercase">
+                      {credit.role[language] || credit.role.es}:{" "}
+                    </span>
+                    <span>{credit.name}</span>
+                  </>
                 )}
-
-                <span>{credit.name}</span>
+                {credit.role?.es && isMobile && (
+                  <div className="mb-1 flex flex-col">
+                    <span className="font-mono text-xs uppercase">
+                      {credit.role[language] || credit.role.es}
+                    </span>
+                    <span>{credit.name}</span>
+                  </div>
+                )}
               </div>
             ))}
         </div>
