caef4c80b74b10bbefdb7c1ab252842e63469110
astrosuka <yegresvoselok@gmail.com>
2025-11-27 23:53:06 -0300

    Remove loader

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,11 +1,5 @@
-import { Loader } from "lucide-react";
-
 export default function Laoding() {
   return (
-    <div className="fixed inset-0 flex h-screen w-full items-center justify-center">
-      <div className="animate-spin">
-        <Loader />
-      </div>
-    </div>
+    <div className="fixed inset-0 flex h-screen w-full items-center justify-center"></div>
   );
 }
