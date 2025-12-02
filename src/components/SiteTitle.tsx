6e075200429ff61145e911fc6ea34f7f5001643e
astrosuka <yegresvoselok@gmail.com>
2025-12-01 21:27:13 -0300

    Make subtitle font thin

--- src/components/SiteTitle.tsx
+++ src/components/SiteTitle.tsx
@@ -2,17 +2,17 @@ import { motion } from "framer-motion";
 
 export default function SiteTitle() {
   return (
-    <motion.div className="text-background absolute flex h-screen w-full flex-col items-center justify-start font-black">
+    <motion.div className="text-background absolute flex h-screen w-full flex-col items-center justify-start">
       <motion.h1
         initial={{ y: 400 }}
         animate={{ y: [400, 400, 0] }}
         transition={{ duration: 3, ease: "easeOut", times: [0, 0.75, 1] }}
-        className="font-display text-blue _text-[6.5vw] _bg-white/80 flex w-full gap-4 text-[7vw] leading-none tracking-tight uppercase [font-variation-settings:'opsz'_80]"
+        className="font-display text-blue _text-[6.5vw] _bg-white/80 flex w-full gap-4 text-[7vw] leading-none font-black tracking-tight uppercase [font-variation-settings:'opsz'_80]"
       >
         <StaggeredText text="Unai Maria de Amorrortu" />
       </motion.h1>
       <div className="w-full overflow-hidden">
-        <motion.h2 className="text-blue flex w-full justify-between px-0.5 text-sm uppercase sm:px-2 sm:text-base">
+        <motion.h2 className="text-blue flex w-full justify-between px-0.5 text-sm uppercase sm:px-2 sm:text-lg">
           <motion.span
             initial={{ y: "-100%" }}
             animate={{ y: 0 }}
